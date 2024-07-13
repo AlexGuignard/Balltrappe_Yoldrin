f7093e30eae81d22ee2a7f39f9ac83f942a528ba
Laynord <alexbtssio@outlook.fr>
2024-07-14 01:46:05 +0200

    add config and missing icon

---
+++ includes/helpers/config.php
@@ -0,0 +1,14 @@
+<?php
+
+//database stuff
+$dbip = "localhost";
+$dbname = "balltrap_yoldrin";
+$dbpsswd = "root";
+$dbuser = "root";
+
+//navbar stuff
+$navbar_color = "#808080";
+$navbar_accent_color = "#a9a9a9";
+$navbar_hover_accent_color = "#5d5b5b";
+
+?>
