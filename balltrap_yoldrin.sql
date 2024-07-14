8f500c6f3a0cf54ab73fe9c75e6721f15393d876
Laynord <alexbtssio@outlook.fr>
2024-07-14 20:59:59 +0200

    update db file

--- balltrap_yoldrin.sql
+++ balltrap_yoldrin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 13, 2024 at 03:03 PM
+-- Generation Time: Jul 14, 2024 at 06:59 PM
 -- Server version: 5.7.39
 -- PHP Version: 8.1.10
 
@@ -24,6 +24,38 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `hunting_records`
+--
+
+CREATE TABLE `hunting_records` (
+  `id` int(11) NOT NULL,
+  `owner_id` int(11) NOT NULL,
+  `category` varchar(8192) NOT NULL,
+  `name` varchar(8192) NOT NULL,
+  `count` int(11) NOT NULL,
+  `weight` int(11) NOT NULL,
+  `tag_number` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `from_id` int(11) NOT NULL,
+  `to_id` int(11) NOT NULL,
+  `time` timestamp NOT NULL,
+  `msg_read` tinyint(1) DEFAULT NULL,
+  `content` varchar(8192) NOT NULL,
+  `attachements` varchar(8192) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -32,15 +64,30 @@ CREATE TABLE `users` (
   `login` varchar(512) NOT NULL,
   `psswd` varchar(128) NOT NULL,
   `admin` tinyint(1) NOT NULL,
-  `email` varchar(512) DEFAULT NULL
+  `email` varchar(512) DEFAULT NULL,
+  `user_icon` varchar(256) NOT NULL DEFAULT 'default_icon.png'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `login`, `psswd`, `admin`, `email`) VALUES
-(1, 'laynord', '9487aa260fd59deb74dd9cbba5598cdc29a4021faf07f9d1d3b0534d89365670', 1, 'alexbtssio@outlook.fr');
+INSERT INTO `users` (`id`, `login`, `psswd`, `admin`, `email`, `user_icon`) VALUES
+(1, 'Laynord', '9487aa260fd59deb74dd9cbba5598cdc29a4021faf07f9d1d3b0534d89365670', 1, 'alexbtssio@outlook.fr', 'G5ihAsyL.png'),
+(2, 'Laynord', '9487aa260fd59deb74dd9cbba5598cdc29a4021faf07f9d1d3b0534d89365670', 1, 'alexbtssio@outlook.fr', 'G5ihAsyL.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_images`
+--
+
+CREATE TABLE `user_images` (
+  `id` int(11) NOT NULL,
+  `file_name` varchar(8192) NOT NULL,
+  `allowed_users` varchar(8192) NOT NULL,
+  `creator_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -58,7 +105,6 @@ CREATE TABLE `wl_emails` (
 --
 
 INSERT INTO `wl_emails` (`id`, `email`) VALUES
-(1, 'lol1963203@gmail.com'),
 (2, 'alexbtssio@outlook.fr');
 
 --
@@ -66,20 +112,56 @@ INSERT INTO `wl_emails` (`id`, `email`) VALUES
 --
 
 --
+-- Indexes for table `hunting_records`
+--
+ALTER TABLE `hunting_records`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `owner_id` (`owner_id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `from_id` (`from_id`,`to_id`),
+  ADD KEY `to_id` (`to_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `email` (`email`);
+
+--
+-- Indexes for table `user_images`
+--
+ALTER TABLE `user_images`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `creator_id` (`creator_id`);
 
 --
 -- Indexes for table `wl_emails`
 --
 ALTER TABLE `wl_emails`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `hunting_records`
+--
+ALTER TABLE `hunting_records`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -88,10 +170,45 @@ ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `user_images`
+--
+ALTER TABLE `user_images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `wl_emails`
 --
 ALTER TABLE `wl_emails`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `hunting_records`
+--
+ALTER TABLE `hunting_records`
+  ADD CONSTRAINT `hunting_records_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`from_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`to_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`email`) REFERENCES `wl_emails` (`email`);
+
+--
+-- Constraints for table `user_images`
+--
+ALTER TABLE `user_images`
+  ADD CONSTRAINT `user_images_ibfk_1` FOREIGN KEY (`creator_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
